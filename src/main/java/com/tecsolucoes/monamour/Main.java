5b5b48b3e81d200f086e34cbc8fc74cee7d6d2bd
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-03-28 17:20:47 -0300

    Logo adicionado a aplicação

--- src/main/java/com/tecsolucoes/monamour/Main.java
+++ src/main/java/com/tecsolucoes/monamour/Main.java
@@ -3,6 +3,7 @@ package com.tecsolucoes.monamour;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,11 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("tela_inicio.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("MonAmour!");
+        stage.setTitle("ArtCraftERP - MonAmour!");
         stage.setScene(scene);
         stage.show();
+
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("Imagens/Logo.png")));
     }
 
     public static void main(String[] args) {
